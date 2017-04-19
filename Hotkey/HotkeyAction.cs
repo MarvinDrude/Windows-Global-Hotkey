e4c37624c643f7dfba104c7ec7dc49205f2794cb
Marvin Drude <marvin_drude@ok.de>
2017-04-19 21:34:58 +0200

    Update HotkeyAction.cs

--- Hotkey/HotkeyAction.cs
+++ Hotkey/HotkeyAction.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PixiColor.Base.Hotkey {
+namespace Base.Hotkeys {
     public class HotkeyAction {
 
         public Hotkey Hotkey { get; set; }
